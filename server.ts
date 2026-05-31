fd30f58582a569f07b1f3bea93bf46e943d6263a
tgk <thienkhoatgk@gmail.com>
2026-06-01 00:00:45 +0700

    cập nhật 0106

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { createServer as createViteServer } from "vite";
 import { GoogleGenAI } from "@google/genai";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
